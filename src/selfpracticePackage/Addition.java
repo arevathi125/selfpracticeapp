b3129642e355a349295fb7614bfb13c9fe4334e6
areva <areva@LAPTOP-EAM40Q36.attlocal.net>
2022-09-29 14:01:15 -0500

    Added one

---
+++ src/selfpracticePackage/Addition.java
@@ -0,0 +1,12 @@
+package selfpracticePackage;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		 String greet = "Hi Jasmine";
+		 String[] arr = greet.split(" ");
+		System.out.println(arr.length);
+
+	}
+
+}
